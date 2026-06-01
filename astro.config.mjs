e005c7b72031cc74a61aa0a2f945a812a30e0cf3
julianbrasse-zhpf <julian.brasse@zhpf.ch>
2026-06-01 19:43:39 +0200

    Temporary fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 import cloudflare from "@astrojs/cloudflare";
 import react from "@astrojs/react";
 import { d1, r2, sandbox } from "@emdash-cms/cloudflare";
-import forms from "@emdash-cms/plugin-forms";
-import webhookNotifier from "@emdash-cms/plugin-webhook-notifier";
+// import forms from "@emdash-cms/plugin-forms";
+// import webhookNotifier from "@emdash-cms/plugin-webhook-notifier";
 import { defineConfig, fontProviders } from "astro/config";
 import emdash from "emdash/astro";
 import { google } from "emdash/auth/providers/google";
@@ -25,8 +25,8 @@ export default defineConfig({
 			authProviders: [google(), github()],
 			database: d1({ binding: "DB", session: "auto" }),
 			storage: r2({ binding: "MEDIA" }),
-			plugins: [forms],
-			sandboxed: [webhookNotifier],
+			plugins: [],
+			sandboxed: [],
 			sandboxRunner: sandbox(),
 			marketplace: "https://marketplace.emdashcms.com",
 		}),
